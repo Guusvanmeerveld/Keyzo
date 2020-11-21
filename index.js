7cf183ec9dd22d6acc2f9c68225d98f1239ae76a
Guus van Meerveld <57101832+Guusvanmeerveld@users.noreply.github.com>
2020-11-22 00:01:58 +0100

    More progress on keybind creation menu

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const { app, BrowserWindow, globalShortcut, shell, autoUpdater } = require('electron');
-const electronLocalshortcut = require('electron-localshortcut');
-const Store = require('electron-store');
+const {
+  app,
+  BrowserWindow,
+  globalShortcut,
+  shell,
+  autoUpdater,
+} = require("electron");
+
+const electronLocalshortcut = require("electron-localshortcut");
+const Store = require("electron-store");
 const store = new Store();
 
 function initApp() {
-    store.set("init", true);
+  store.set("init", true);
 
-    store.set("minimize", true);
-    store.set("startup", true);
+  store.set("minimize", true);
+  store.set("startup", true);
 }
 
-const AutoLaunch = require('auto-launch');
+const AutoLaunch = require("auto-launch");
 
 // const server = 'https://cdn.g-vm.nl'
 // const url = `${server}/update/${process.platform}/${app.getVersion()}`
@@ -19,101 +26,99 @@ const AutoLaunch = require('auto-launch');
 
 let loadingScreen;
 function createLoadingScreen() {
-    loadingScreen = new BrowserWindow({
-        width: 200,
-        height: 400,
-        frame: false,
-        icon: "public/icon/icon.png",
-        backgroundColor: '#212121'
-    });
+  loadingScreen = new BrowserWindow({
+    width: 200,
+    height: 400,
+    frame: false,
+    icon: "public/icon/icon.png",
+    backgroundColor: "#212121",
+  });
 
-    loadingScreen.setResizable(false);
-    loadingScreen.on('closed', () => loadingScreen = null);
-    loadingScreen.loadFile('public/pages/loadscreen.html')
-};
-
-function createWindow() {
-
-    let win = new BrowserWindow({
-        webPreferences: {
-            nodeIntegration: true,
-            webviewTag: true,
-            // enableBlinkFeatures: false,
-            enableRemoteModule: true
-        },
-        frame: false,
-        show: false,
-        titleBarStyle: "hidden",
-        backgroundColor: '#212121',
-        'minWidth': 600,
-        'minHeight': 600,
-        icon: "public/icon/icon.png"
-    })
-
-    // autoUpdater.checkForUpdates()
-    win.loadFile('public/index.html')
-
-    win.webContents.on('new-window', function (e, url) {
-        e.preventDefault();
-        shell.openExternal(url);
-    });
-
-    electronLocalshortcut.register(win, 'Ctrl+R', () => {
-        win.reload()
-        // app.relaunch()
-        // app.exit()
-    });
-
-    electronLocalshortcut.register(win, 'Ctrl+Shift+I', () => {
-        win.webContents.openDevTools()
-    });
-
-    win.on("close", e => {
-        if (store.get('minimize')) {
-            e.preventDefault();
-            if (!app.isQuiting) {
-                win.hide();
-            }
-        }
-    })
-
-    win.once('ready-to-show', () => {
-        if (loadingScreen) {
-            loadingScreen.close();
-        }
-        win.show()
-    })
+  loadingScreen.setResizable(false);
+  loadingScreen.on("closed", () => (loadingScreen = null));
+  loadingScreen.loadFile("public/pages/loadscreen.html");
 }
 
-app.on('ready', () => {
-    createLoadingScreen()
-    createWindow()
+function createWindow() {
+  let win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      webviewTag: true,
+      // enableBlinkFeatures: false,
+      enableRemoteModule: true,
+    },
+    frame: false,
+    show: false,
+    titleBarStyle: "hidden",
+    backgroundColor: "#212121",
+    minWidth: 600,
+    minHeight: 600,
+    icon: "public/icon/icon.png",
+  });
 
-    if (!store.get("init")) {
-        initApp()
+  // autoUpdater.checkForUpdates()
+  win.loadFile("public/index.html");
+
+  win.webContents.on("new-window", function (e, url) {
+    e.preventDefault();
+    shell.openExternal(url);
+  });
+
+  electronLocalshortcut.register(win, "Ctrl+R", () => {
+    win.reload();
+    // app.relaunch()
+    // app.exit()
+  });
+
+  electronLocalshortcut.register(win, "Ctrl+Shift+I", () => {
+    win.webContents.openDevTools();
+  });
+
+  win.on("close", (e) => {
+    if (store.get("minimize")) {
+      e.preventDefault();
+      if (!app.isQuiting) {
+        win.hide();
+      }
     }
+  });
 
-    let autoLaunch = new AutoLaunch({
-        name: 'Keyzo',
-        path: app.getPath('exe'),
-    });
-    
-    if (store.get("startup")) {
-        autoLaunch.enable();
-    } else {
-        autoLaunch.disable();
+  win.once("ready-to-show", () => {
+    if (loadingScreen) {
+      loadingScreen.close();
     }
-})
+    win.show();
+  });
+}
 
+app.on("ready", () => {
+  createLoadingScreen();
+  createWindow();
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
+  if (!store.get("init")) {
+    initApp();
+  }
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
-    }
-})
+  let autoLaunch = new AutoLaunch({
+    name: "Keyzo",
+    path: app.getPath("exe"),
+  });
+
+  if (store.get("startup")) {
+    autoLaunch.enable();
+  } else {
+    autoLaunch.disable();
+  }
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,28 +1,30 @@
-const customTitlebar = require('custom-electron-titlebar');
-const $ = require("./javascripts/jquery.min.js")
+const customTitlebar = require("custom-electron-titlebar");
+const { Menu } = require("electron");
+const $ = require("./javascripts/jquery.min.js");
 
 var titleBar = new customTitlebar.Titlebar({
-    backgroundColor: customTitlebar.Color.fromHex('#292929'),
-    icon: "icon/icon.png"
+  menu: null,
+  backgroundColor: customTitlebar.Color.fromHex("#292929"),
+  icon: "icon/icon.png",
 });
 
 function init() {
-    document.querySelectorAll(".nav-item").forEach(x => {
-        x.addEventListener('click', () => {
-            if ($("#main-page").prop('src').indexOf(x.id) < 0) {
-                $("#main-page").prop('src', `pages/${x.id}.html`);
-                document.querySelectorAll(".nav-item").forEach(y => {
-                    y.classList.remove('active')
-                })
-                x.classList.add('active')
-            };
+  document.querySelectorAll(".nav-item").forEach((x) => {
+    x.addEventListener("click", () => {
+      if ($("#main-page").prop("src").indexOf(x.id) < 0) {
+        $("#main-page").prop("src", `pages/${x.id}.html`);
+        document.querySelectorAll(".nav-item").forEach((y) => {
+          y.classList.remove("active");
         });
+        x.classList.add("active");
+      }
     });
-    document.querySelectorAll(".nav-item")[0].classList.add("active")
-};
+  });
+  document.querySelectorAll(".nav-item")[0].classList.add("active");
+}
 
 document.onreadystatechange = function () {
-    if (document.readyState == "complete") {
-        init();
-    }
+  if (document.readyState == "complete") {
+    init();
+  }
 };
